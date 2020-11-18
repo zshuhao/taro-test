f3b810708a5b49b99e7bda589481567d58d75f18
zhangshuhao <zhang.shuhao@mwee.cn>
2020-11-18 19:16:07 +0800

    fix

--- src/config/app.prod.ts
+++ src/config/app.prod.ts
@@ -1,7 +1,5 @@
 const config = {
-    API_ROOT: 'https://gateway-infc.mwee.cn',
-    API_AUTH: 'https://cas.mweer.com',
-    API_QR: 'https://miaofu-api.mwee.cn/rest/api'
+    
 }
 
 export default config
